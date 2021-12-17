7039ebd23ff670fb89441dc37ad3473fffa5db51
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2021-12-17 18:17:50 +0900

    Post 모델 생성하기

--- FinalProj/shoesmall/models.py
+++ FinalProj/shoesmall/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=30) #상품명
+    hook_text = models.CharField(max_length=30) #간단한 소개
+
+    price = models.CharField(max_length=30) #상품 가격
+
+    # product는 제조사 모델이 작성된 후 작성하기
+    # category는 카테고리 모델이 작성된 후 작성하기
+
+    # tag는 태그 모델이 작성된 후 작성하기
+    # size는 나중에..
+
+    def __str__(self):
+        return f'{self.name}'
