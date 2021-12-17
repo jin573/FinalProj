9cb5df1b9f947c3ca784812b623c1572d8880e56
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2021-12-18 00:17:21 +0900

    shoes_list 목록 페이지 만들기

--- FinalProj/shoesmall/views.py
+++ FinalProj/shoesmall/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    items = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'shoesmall/index.html',
+        {
+            'items':items,
+        }
+
+    )
