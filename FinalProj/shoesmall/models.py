098cec97ae7c0dad163c6fbd3b4b19e2801efad5
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2021-12-19 03:28:38 +0900

    목록 페이지에 카테고리 추가

--- FinalProj/shoesmall/models.py
+++ FinalProj/shoesmall/models.py
@@ -3,6 +3,24 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    address = models.CharField(max_length=30, unique=True)
+    phone = models.CharField(max_length=30, unique=True)
+
+    slug = slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     item = models.CharField(max_length=30)
@@ -10,9 +28,9 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='shoesmall/images/%Y/%m/%d/', blank=True)
     price = models.CharField(max_length=20)
     #제조사
-    product = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     #카테고리
-
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
     #tag
     msg = models.BooleanField(default=True)
     restock = models.DateField(null=True)
