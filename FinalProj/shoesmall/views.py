ef752de25825aa5109e25aea69f87a4a01f81db5
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2021-12-18 23:29:27 +0900

    목록과 상세 페이지 부트스트랩 적용

--- FinalProj/shoesmall/views.py
+++ FinalProj/shoesmall/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 #def index(request):
 #    items = Post.objects.all().order_by('-pk')
@@ -18,13 +20,13 @@ class PostList(ListView):
 
 #    )
 
-def single_item_page(request,pk):
-    item = Post.objects.get(pk=pk)
+#def single_item_page(request,pk):
+#    item = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'shoesmall/single_item_page.html',
-        {
-            'item':item,
-        }
-    )
+#    return render(
+#        request,
+#        'shoesmall/post_detail.html',
+#        {
+#            'item':item,
+#        }
+#    )
